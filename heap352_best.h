e53b06df7b7c7e971aa1fd9ec472fa610e0a5522
donjuma <Omar.Juma@wwu.edu>
2015-04-27 18:28:04 -0700

    Finished :/ ....after 13 hours non stop. Fuck u bro...

---
+++ heap352_best.h
@@ -0,0 +1,14 @@
+/*  heap352.h
+
+    Header file for heap management library routines
+    CSCI 352, Assignment 1
+    Spring, 2015
+
+    David Bover, April, 2015
+
+*/
+
+void *malloc352 (int size);
+
+
+void free352 (void *ptr);
